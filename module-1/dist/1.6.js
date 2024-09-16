6b05127bc7bdccdb9fa4d8f5da122989151684f3
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2024-09-16 22:24:38 +0600

    function

---
+++ module-1/dist/1.6.js
@@ -0,0 +1,9 @@
+"use strict";
+// function
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(1, 2);
+
+
+console.log(add);
---
+++ module-1/src/1.6.ts
@@ -0,0 +1,12 @@
+
+   // function
+
+   function add(num1: number, num2: number): number{
+      return num1 + num2;
+   }
+
+   add(1,2);
+   console.log(add)
+   
+
+
