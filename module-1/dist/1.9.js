ec5ee2bd8f7fbd3525d8af409ef5c1d20e96304e
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2024-09-16 22:45:54 +0600

    type alias

---
+++ module-1/dist/1.9.js
@@ -0,0 +1,10 @@
+"use strict";
+{
+    var student1 = {
+        name: "Raisul",
+        age: 23,
+        address: "natore",
+        id: 1,
+        married: false,
+    };
+}
---
+++ module-1/src/1.9.ts
@@ -0,0 +1,19 @@
+{
+   // type alias
+  type Student = {
+      name : string,
+      age : number,
+      address: string,
+      id: number,
+      contact?: number,
+      married: boolean
+   }
+
+   const student1 : Student ={
+      name : "Raisul",
+      age: 23,
+      address: "natore",
+      id: 1,
+      married: false,
+   }
+}
